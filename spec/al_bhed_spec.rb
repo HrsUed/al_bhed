49fc92eaeaf0c363deb9e87035b5392861221ab9
HrsUed <hiroshi.ueda.career.9756@gmail.com>
2020-05-29 22:42:12 +0900

    add tests for String#to_albhed

--- spec/al_bhed_spec.rb
+++ spec/al_bhed_spec.rb
@@ -1,9 +1,19 @@
 RSpec.describe AlBhed do
-  it "has a version number" do
-    expect(AlBhed::VERSION).not_to be nil
-  end
+  describe "String#to_albhed" do
+    it "should translate downcased latin alphabets into Al Bheds" do
+      results = "abcdefghijklmnopqrstuvwxyz".to_albhed
+      expect(results).to eq "ypltavkrezgmshubxncdijfqow"
+    end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+    it "should translate upcased latin alphabets into Al Bheds" do
+      results = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".to_albhed
+      expect(results).to eq "YPLTAVKREZGMSHUBXNCDIJFQOW"
+    end
+
+    it "should keep it if no latin alphabets" do
+      no_latins = "1234567890,. !?"
+      results = no_latins.to_albhed
+      expect(results).to eq no_latins
+    end
   end
 end
