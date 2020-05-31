5ab8921e6908efe7f83c9d8a67b249f2e9ab9932
HrsUed <hiroshi.ueda.career.9756@gmail.com>
2020-06-01 08:57:20 +0900

    remove useless text from examples

--- spec/al_bhed_spec.rb
+++ spec/al_bhed_spec.rb
@@ -14,154 +14,154 @@ RSpec.describe AlBhed do
 
     context "translate Japanese" do
       context "Hira Kana" do
-        it "should translate あ行 Kana into Al Bheds" do
+        it "should translate あ行 into Al Bheds" do
           results = "あいうえお".to_albhed
           expect(results).to eq "ワミフネト"
         end
 
-        it "should translate か行 Kana into Al Bheds" do
+        it "should translate か行 into Al Bheds" do
           results = "かきくけこ".to_albhed
           expect(results).to eq "アチルテヨ"
         end
 
-        it "should translate さ行 Kana into Al Bheds" do
+        it "should translate さ行 into Al Bheds" do
           results = "さしすせそ".to_albhed
           expect(results).to eq "ラキヌヘホ"
         end
 
-        it "should translate た行 Kana into Al Bheds" do
+        it "should translate た行 into Al Bheds" do
           results = "たちつてと".to_albhed
           expect(results).to eq "サヒユセソ"
         end
 
-        it "should translate な行 Kana into Al Bheds" do
+        it "should translate な行 into Al Bheds" do
           results = "なにぬねの".to_albhed
           expect(results).to eq "ハシスメオ"
         end
 
-        it "should translate は行 Kana into Al Bheds" do
+        it "should translate は行 into Al Bheds" do
           results = "はひふへほ".to_albhed
           expect(results).to eq "マリクケロ"
         end
 
-        it "should translate ま行 Kana into Al Bheds" do
+        it "should translate ま行 into Al Bheds" do
           results = "まみむめも".to_albhed
           expect(results).to eq "ヤイツレコ"
         end
 
-        it "should translate や行 Kana into Al Bheds" do
+        it "should translate や行 into Al Bheds" do
           results = "やゆよ".to_albhed
           expect(results).to eq "タヲモ"
         end
 
-        it "should translate ら行 Kana into Al Bheds" do
+        it "should translate ら行 into Al Bheds" do
           results = "らりるれろ".to_albhed
           expect(results).to eq "ナニウエノ"
         end
 
-        it "should translate わ行 Kana into Al Bheds" do
+        it "should translate わ行 into Al Bheds" do
           results = "わをん".to_albhed
           expect(results).to eq "カムン"
         end
 
-        it "should translate が行 Kana into Al Bheds" do
+        it "should translate が行 into Al Bheds" do
           results = "がぎぐげご".to_albhed
           expect(results).to eq "ダジヅデゾ"
         end
 
-        it "should translate ざ行 Kana into Al Bheds" do
+        it "should translate ざ行 into Al Bheds" do
           results = "ざじずぜぞ".to_albhed
           expect(results).to eq "バギブゲボ"
         end
 
-        it "should translate だ行 Kana into Al Bheds" do
+        it "should translate だ行 into Al Bheds" do
           results = "だぢづでど".to_albhed
           expect(results).to eq "ガビグベゴ"
         end
 
-        it "should translate ば行 Kana into Al Bheds" do
+        it "should translate ば行 into Al Bheds" do
           results = "ばびぶべぼ".to_albhed
           expect(results).to eq "ザヂズゼド"
         end
 
-        it "should translate ぱ行 Kana into Al Bheds" do
+        it "should translate ぱ行 into Al Bheds" do
           results = "ぱぴぷぺぽ".to_albhed
           expect(results).to eq "プペパポピ"
         end
       end
 
       context "Kata Kana" do
-        it "should translate ア行 Kana into Al Bheds" do
+        it "should translate ア行 into Al Bheds" do
           results = "アイウエオ".to_albhed
           expect(results).to eq "ワミフネト"
         end
 
-        it "should translate カ行 Kana into Al Bheds" do
+        it "should translate カ行 into Al Bheds" do
           results = "カキクケコ".to_albhed
           expect(results).to eq "アチルテヨ"
         end
 
-        it "should translate サ行 Kana into Al Bheds" do
+        it "should translate サ行 into Al Bheds" do
           results = "サシスセソ".to_albhed
           expect(results).to eq "ラキヌヘホ"
         end
 
-        it "should translate タ行 Kana into Al Bheds" do
+        it "should translate タ行 into Al Bheds" do
           results = "タチツテト".to_albhed
           expect(results).to eq "サヒユセソ"
         end
 
-        it "should translate ナ行 Kana into Al Bheds" do
+        it "should translate ナ行 into Al Bheds" do
           results = "ナニヌネノ".to_albhed
           expect(results).to eq "ハシスメオ"
         end
 
-        it "should translate ハ行 Kana into Al Bheds" do
+        it "should translate ハ行 into Al Bheds" do
           results = "ハヒフヘホ".to_albhed
           expect(results).to eq "マリクケロ"
         end
 
-        it "should translate マ行 Kana into Al Bheds" do
+        it "should translate マ行 into Al Bheds" do
           results = "マミムメモ".to_albhed
           expect(results).to eq "ヤイツレコ"
         end
 
-        it "should translate ヤ行 Kana into Al Bheds" do
+        it "should translate ヤ行 into Al Bheds" do
           results = "ヤユヨ".to_albhed
           expect(results).to eq "タヲモ"
         end
 
-        it "should translate ラ行 Kana into Al Bheds" do
+        it "should translate ラ行 into Al Bheds" do
           results = "ラリルレロ".to_albhed
           expect(results).to eq "ナニウエノ"
         end
 
-        it "should translate ワ行 Kana into Al Bheds" do
+        it "should translate ワ行 into Al Bheds" do
           results = "ワヲン".to_albhed
           expect(results).to eq "カムン"
         end
 
-        it "should translate ガ行 Kana into Al Bheds" do
+        it "should translate ガ行 into Al Bheds" do
           results = "ガギグゲゴ".to_albhed
           expect(results).to eq "ダジヅデゾ"
         end
 
-        it "should translate ザ行 Kana into Al Bheds" do
+        it "should translate ザ行 into Al Bheds" do
           results = "ザジズゼゾ".to_albhed
           expect(results).to eq "バギブゲボ"
         end
 
-        it "should translate ダ行 Kana into Al Bheds" do
+        it "should translate ダ行 into Al Bheds" do
           results = "ダヂヅデド".to_albhed
           expect(results).to eq "ガビグベゴ"
         end
 
-        it "should translate バ行 Kana into Al Bheds" do
+        it "should translate バ行 into Al Bheds" do
           results = "バビブベボ".to_albhed
           expect(results).to eq "ザヂズゼド"
         end
 
-        it "should translate パ行 Kana into Al Bheds" do
+        it "should translate パ行 into Al Bheds" do
           results = "パピプペポ".to_albhed
           expect(results).to eq "プペパポピ"
         end
